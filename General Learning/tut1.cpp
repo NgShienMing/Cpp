b4f312fff54dc069211aa5904904f1ba0e634242
ngshienming <nsming1030@gmail.com>
2022-03-11 09:41:51 +0800

    ece2071 tutorial 1

---
+++ General Learning/tut1.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    int i, j;
+    for(i=1; i<=5; ++i)
+    {
+        for(j=0; j<i; ++j)
+        printf("%3d ", j);
+        printf("\n");
+    }
+    for(i=4; i>0; --i)
+    {
+        for(j=0; j<i; ++j)
+        printf("%3d ", j);
+        printf("\n");
+    }
+}
